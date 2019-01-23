4c22fb60f8155e4e0de28cac80fc9348c7dc0966
georgiaDabs <gdabinett@gmail.com>
2019-01-23 13:47:37 +0000

    rendering

--- server.js
+++ server.js
@@ -109,11 +109,26 @@ var recipes={"Spaghetti":{
 }
 };
 var people={"doctorwhocomposer":{"username":"doctorwhocomposer", "forename":"Delia", "surname":"Derbyshire"}};
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-  });
+app.get('/', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      if (err) {
+        console.log('Error running count. Message:\n'+err);
+      }
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
+});
 app.all('/allrecipes',function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
