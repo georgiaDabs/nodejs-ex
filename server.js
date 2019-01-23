667415c0707c2133f74fd8d926659560a5c75aff
georgiaDabs <gdabinett@gmail.com>
2019-01-23 13:16:23 +0000

    got rid of duplicated express

--- server.js
+++ server.js
@@ -73,9 +73,8 @@ var initDb = function(callback) {
   });
 };
 
-const express=require('express');
 const bodyParser=require('body-parser');
-const app=express();
+
 app.use(bodyParser.urlencoded({    
   extended: true
 })); 
